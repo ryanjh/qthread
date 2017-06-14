c7a3fb8ca1bf58dd51808e808e02d9c93349c26a
Ryan Chu <juic@cypress.com>
2017-06-14 09:07:37 +0000

    Enable thread network as a leader by default

--- qt_console/main.cpp
+++ qt_console/main.cpp
@@ -1,4 +1,5 @@
 #include <QCoreApplication>
+#include <QThread>
 #include <iostream>
 #include <qt_console.h>
 
@@ -33,8 +34,8 @@ int main(int argc, char *argv[])
     if (argc < 2)
     {
         char *argv_default[2] = {argv[0], (char*) "1"};
-
         PlatformInit(2, argv_default);
+        printf("PlatformInit by default!\n");
     }
     else
     {
@@ -43,6 +44,53 @@ int main(int argc, char *argv[])
 
     otInstance *sInstance = otInstanceInit();
     assert(sInstance);
+
+    printf("otLinkSetPanId (%d)\n", otLinkSetPanId(sInstance, static_cast<otPanId>(0x1234)));
+    printf("panid = 0x%x\n", otLinkGetPanId(sInstance));
+
+    if (!otIp6IsEnabled(sInstance))
+    {
+        printf("otIp6SetEnabled (%d)\n", otIp6SetEnabled(sInstance, true));
+    }
+    printf("ifconfig up\n");
+
+    printf("otThreadSetEnabled (%d)\n", otThreadSetEnabled(sInstance, true));
+    printf("thread start\n");
+
+    for (int i = 0; i < 20; i++)
+    {
+        printf("waiting thread network ... (%d)\n", i);
+        QThread::sleep(1);
+        otTaskletsProcess(sInstance);
+        PlatformProcessDrivers(sInstance);
+
+        switch (otThreadGetDeviceRole(sInstance))
+        {
+        case OT_DEVICE_ROLE_DISABLED:
+            printf("state disabled\n");
+            break;
+
+        case OT_DEVICE_ROLE_DETACHED:
+            printf("state detached\n");
+            break;
+
+        case OT_DEVICE_ROLE_CHILD:
+            printf("state child\n");
+            break;
+
+        case OT_DEVICE_ROLE_ROUTER:
+            printf("state router\n");
+            break;
+
+        case OT_DEVICE_ROLE_LEADER:
+            printf("state leader\n");
+            break;
+
+        default:
+            printf("invalid state\n");
+            break;
+        }
+    }
 
     return a.exec();
 }
